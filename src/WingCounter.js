1a8e8a97da1bd665ccfd193707b5d71becb12158
Gurjit Sangha <gurjit.sangha@sky.uk>
2016-09-16 16:51:34 +0100

    added random chicken wings

--- src/WingCounter.js
+++ src/WingCounter.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
-import wing from '../media/fried.png'
+import wing from '../media/fried.png';
 
 class WingCounter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      wingCounter: 0
+      wingCounter: 0,
+      wingStyles: []
     }
+    this.spawnWing = this.spawnWing.bind(this);
     this.incrementer = this.incrementer.bind(this)
   }
+
+  spawnWing() {
+    const rTop = Math.random() * window.innerHeight;
+    const rLeft = Math.random() * window.innerWidth;
+    const style = {
+      position: 'absolute',
+      left: `${rLeft}px`,
+      top: `${rTop}px`
+    };
+    this.setState({
+      wingStyles: this.state.wingStyles.concat([style])
+    })
+  }
+
   incrementer() {
     this.setState({
       wingCounter: this.state.wingCounter+1
     })
+    this.spawnWing();
   }
+
   render() {
     return (
-      <div className="center">
-      <div className="Title">
-        <p>{this.state.wingCounter}</p>
-        <img className="WingImg" src={wing} />
-      </div>
       <div>
-        <button type="button" onClick={this.incrementer}>+</button>
-      </div>
+        {
+          this.state.wingStyles.map((wingStyle, i) => {
+            return <img src={wing} key={i} style={wingStyle} className='spawn-chicken'/>
+          })
+        }
+        <div className="center">
+          <div className="Title">
+            <p>{this.state.wingCounter}</p>
+            <img className="WingImg" src={wing} />
+          </div>
+          <div>
+            <button type="button" onClick={this.incrementer}>+</button>
+          </div>
+        </div>
       </div>
     );
   }
